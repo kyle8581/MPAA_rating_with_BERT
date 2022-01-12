627000f901c1bb66943a95fc2fa0ec9d261a7b65
kyle8581 <mapoout@naver.com>
2022-01-12 15:55:32 +0900

    wandb logger added

--- src/train.py
+++ src/train.py
@@ -1,3 +1,20 @@
+import wandb
+from tqdm import tqdm
+from dataloader import get_dataloader
+import warnings
+from sklearn.metrics import f1_score, recall_score, precision_score
+from tensorboardX import SummaryWriter
+from models.model import Rater
+from utils.util import TorchHelper
+from torch.nn import functional as F
+from torch import optim
+from random import shuffle
+import pandas as pd
+import numpy as np
+import json
+import time
+from torch import nn
+import random
 import torch
 from torch.utils.data import DataLoader
 
@@ -11,7 +28,6 @@ sys.path.append('../')
 sys.path.append("../data")
 # os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 # os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
-import random
 
 # fix random seed as 7
 torch.manual_seed(7)
@@ -21,49 +37,37 @@ torch.backends.cudnn.enabled = False
 
 random.seed(7)
 
-from torch import nn
-import time
-import json
-import numpy as np
-import pandas as pd
-from random import shuffle
-from torch import optim
-from torch.nn import functional as F
-
-from utils.util import TorchHelper
-
-from models.model import Rater
-from tensorboardX import SummaryWriter
 
 # for metrics
-from sklearn.metrics import f1_score, recall_score, precision_score
-import warnings
 
-from dataloader import get_dataloader
-from tqdm import tqdm
 
 warnings.filterwarnings('ignore')
 
 
-
 parser = argparse.ArgumentParser(description='MPAA rating with BERT.')
-parser.add_argument('batch_size', type=int, 
+parser.add_argument('batch_size', type=int,
                     help='(int) batch size', default=16)
 
-parser.add_argument('max_epochs', type=int, help='(int) max number of epochs', default=60)
+parser.add_argument('max_epochs', type=int,
+                    help='(int) max number of epochs', default=60)
 
+parser.add_argument("logger_type", type=str,
+                    help="wandb or tensorboard", default="tensorboard")
+
+parser.add_argument(
+    "lr", type=float, help="initial learning rate", default=1e-6)
 
 
 args = parser.parse_args()
 
 
-device = "cuda"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 batch_size = args.batch_size
+logger_type = args.logger_type
 
 
 loss_weights1 = torch.Tensor([0.5669, 0.2913, 0.1418])
-
 
 
 # run_mode = 'run'
@@ -77,22 +81,38 @@ start_epoch = 0
 
 
 max_epochs = args.max_epochs
-learning_rate = 0.000001
+learning_rate = args.lr
 clip_grad = 0.5
 weight_decay_val = 0
 optimizer_type = 'adam'  # sgd
 
-base_log_dir = "/home/chaehyeong/MARS_hj/BERT_rating/results/log"
-log_dir = os.path.join(base_log_dir, f"lr_{learning_rate}_batch_size{batch_size}_")
-summary = SummaryWriter(log_dir="/home/chaehyeong/MARS_hj/BERT_rating/results/log")
+base_dir = "/home/chaehyeong/MARS_hj/BERT_rating"
+base_log_dir = os.path.join(base_dir, "results/log")
+log_dir = os.path.join(
+    base_log_dir, f"lr_{learning_rate}_batch_size{batch_size}_")
+summary = SummaryWriter(log_dir=os.path.join(base_dir, "results/log"))
 
 
-partition_dict = json.load(open("/home/chaehyeong/MARS_hj/BERT_rating/data/partition.json", 'r'))
+partition_dict = json.load(
+    open(os.path.join(base_dir, "data/partition.json", 'r')))
 
+if logger_type == "wandb":
+    wandb.init()
+    wandb.init(project="MPAA_rating_with_BERT")
+
+    wandb.config.batch_size = batch_size
+    wandb.config.learning_rate = learning_rate
+
+    wandb.define_metric("train_step")
+    wandb.define_metric("epoch")
+    wandb.define_metric(
+        "train_step/*", step_metric="train_step", summary="min")
+    wandb.define_metric("train_epoch/*", step_metric="epoch")
+    wandb.define_metric("val_epoch/*", step_metric="epoch", summary="max")
 
 train_id_list = partition_dict['train']
 val_id_list = partition_dict['val']
-test_id_list= partition_dict['test']
+test_id_list = partition_dict['test']
 
 
 # Learning rate scheduler
@@ -103,12 +123,9 @@ reduce_on_plateau_lr_schdlr = torch.optim.lr_scheduler.ReduceLROnPlateau
 run_name = f'rating_with_BERT_lr{learning_rate}/'
 description = ''
 
-output_dir_path = '/home/chaehyeong/MARS_hj/BERT_rating/results'+ run_name
+output_dir_path = os.path.join(base_dir, 'results', run_name)
 if not os.path.exists(output_dir_path):
     os.mkdir(output_dir_path)
-
-
-
 
 
 total_train_step_count = 0
@@ -135,10 +152,6 @@ def create_model(device, mode):
     return model
 
 
-
-
-
-
 # ----------------------------------------------------------------------------
 # Training loop
 # ----------------------------------------------------------------------------
@@ -158,15 +171,11 @@ def train(model, optimizer, dataloader, device):
     batch_idx = 1
     total_loss = 0
 
-    
-
     for step, batch in enumerate(tqdm(dataloader)):
 
         x, y = batch["x"].to(device), batch["y"]
 
-
         out = model(x)
-
 
         y_pred1 = out.cpu()
 
@@ -176,31 +185,30 @@ def train(model, optimizer, dataloader, device):
             for k, v in batch.items():
                 print(f"{k} : {v}")
 
-
         print(f"cross entropy loss : {ce_loss}")
         loss = ce_loss
-        
 
         total_loss += loss.item()
 
         loss.backward()
 
-
         optimizer.step()
 
         torch_helper.show_progress(batch_idx, np.ceil(len(train_id_list) / batch_size), start_time,
-                                    round(total_loss / (step + 1), 4))
+                                   round(total_loss / (step + 1), 4))
 
-       
         batch_idx += 1
-      
-        
+
         total_train_step_count += 1
         if total_train_step_count % 10 == 0:
-            summary.add_scalar("train_ce_loss", ce_loss.cpu().item(), total_train_step_count)
-            summary.add_scalar("train_total_loss", loss.cpu().item(), total_train_step_count)
-
-
+            if logger_type == "tensorboard":
+                summary.add_scalar(
+                    "train_ce_loss", ce_loss.cpu().item(), total_train_step_count)
+                summary.add_scalar("train_total_loss",
+                                   loss.cpu().item(), total_train_step_count)
+            if logger_type == "wandb":
+                wandb.log({"train_step/ce_loss": ce_loss.cpu().item(),
+                          "train_step": total_train_step_count})
 
     return model
 
@@ -208,27 +216,24 @@ def train(model, optimizer, dataloader, device):
 # ----------------------------------------------------------------------------
 # Evaluate the model
 # ----------------------------------------------------------------------------
-def evaluate(model, dataloader, device):
+def evaluate(model, dataloader, device, epoch, logger_prefix):
     model.eval()
 
-    total_loss1  = 0 
+    total_loss1 = 0
 
     y1_true, y2_true, y3_true = [], [], []
 
     predictions = [[], [], []]
-    
+
     with torch.no_grad():
         for step, batch in enumerate(tqdm(dataloader)):
 
             x, y = \
-            batch["tokenized_script"].to(device), batch["rating"]
-
+                batch["tokenized_script"].to(device), batch["rating"]
 
             out = model(x)
 
             y_pred1 = out.cpu()
-
-
 
             predictions[0].extend(list(torch.argmax(y_pred1, -1).numpy()))
             y1_true.extend(list(y.numpy()))
@@ -236,35 +241,37 @@ def evaluate(model, dataloader, device):
             loss2 = F.cross_entropy(y_pred1, y)
             total_loss1 += loss2.item()
 
-
-
     micro_f1_2 = f1_score(y1_true, predictions[0], average='weighted')
-    print ("****************")
+    average_type = ["micro", "macro", "weighted"]
+    f1 = {k: f1_score(y1_true, predictions[0], average=k)
+          for k in average_type}
 
-    #================== micro ====================================
-    print('f1 mirco :', f1_score(y1_true, predictions[0], average='micro'))
-    print(f'precision micro : {precision_score(y1_true, predictions[0], average="micro")}')
-    print(f'recall micro: {recall_score(y1_true, predictions[0], average="micro")}')
-    print ("****************")
+    precision = {k: precision_score(
+        y1_true, predictions[0], average=k) for k in average_type}
 
-    #================== macro ====================================
-    print('f1 macro :', f1_score(y1_true, predictions[0], average='macro'))
-    print(f'precision macro : {precision_score(y1_true, predictions[0], average="macro")}')
-    print(f'recall macro: {recall_score(y1_true, predictions[0], average="macro")}')
+    recall = {k: recall_score(
+        y1_true, predictions[0], average=k) for k in average_type}
 
-    print ("****************")
-    #================== weighted =================================
-    print(f'f1 weighted : {micro_f1_2}')
-    print(f'precision weighted : {precision_score(y1_true, predictions[0], average="weighted")}')
-    print(f'recall weighted: {recall_score(y1_true, predictions[0], average="weighted")}')
+    for t in average_type:
+        print("****************")
+        print(f"f1 {t} : {f1[t]}")
+        print(f"precision {t} : {precision[t]}")
+        print(f"recall {t} : {recall[t]}")
 
-    print ("****************")
+    print("****************")
+    if logger_type == "wandb":
+        for t in average_type:
+            wandb.log({
+                f"{logger_prefix}/f1_{t}": f1[t],
+                f"{logger_prefix}/recall_{t}": recall[t],
+                f"{logger_prefix}/precision_{t}": precision[t],
+                "epoch": epoch
 
+            })
 
     return predictions, \
-           total_loss1 / len(dataloader), \
-           micro_f1_2
-   
+        total_loss1 / len(dataloader), \
+        micro_f1_2
 
 
 def training_loop(batch_size, device):
@@ -284,23 +291,26 @@ def training_loop(batch_size, device):
     if optimizer_type == 'adam':
         optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     elif optimizer_type == 'sgd':
-        optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=weight_decay_val)
+        optimizer = optim.SGD(model.parameters(
+        ), lr=learning_rate, momentum=0.9, weight_decay=weight_decay_val)
 
-    lr_scheduler = reduce_on_plateau_lr_schdlr(optimizer, 'max', min_lr=1e-8, patience=2, factor=0.5)
+    lr_scheduler = reduce_on_plateau_lr_schdlr(
+        optimizer, 'max', min_lr=1e-8, patience=2, factor=0.5)
 
     for epoch in range(start_epoch, max_epochs):
         print('[Epoch %d] / %d : %s' % (epoch + 1, max_epochs, run_name))
- 
+
         model = train(model, optimizer, train_dataloader, device)
 
-        val_pred, val_loss1, val_f1 = evaluate(model, val_dataloader, device)
-        train_pred, train_loss1, train_f1 = evaluate(model, train_dataloader, device)
-
-        summary.add_scalar("train_loss_epoch", train_loss1, epoch)
-        summary.add_scalar("train_f1_epoch", train_f1, epoch)
-        summary.add_scalar("val_loss", val_loss1, epoch)
-        summary.add_scalar("val_f1", val_f1, epoch)
-
+        val_pred, val_loss1, val_f1 = evaluate(
+            model, val_dataloader, device, epoch, "val_epoch")
+        train_pred, train_loss1, train_f1 = evaluate(
+            model, train_dataloader, device, epoch, "train_epoch")
+        if logger_type == "tensorboard":
+            summary.add_scalar("train_loss_epoch", train_loss1, epoch)
+            summary.add_scalar("train_f1_epoch", train_f1, epoch)
+            summary.add_scalar("val_loss", val_loss1, epoch)
+            summary.add_scalar("val_f1", val_f1, epoch)
 
         current_lr = 0
         for pg in optimizer.param_groups:
@@ -316,7 +326,6 @@ def training_loop(batch_size, device):
         is_best = torch_helper.checkpoint_model(model, optimizer, output_dir_path, val_f1, epoch + 1,
                                                 'max')
 
-
         print()
 
         # -------------------------------------------------------------
@@ -328,6 +337,8 @@ def training_loop(batch_size, device):
                 'val_f1_1': val_f1,
                 'lr': current_lr
                 }
+
+
 def test(batch_size, device):
     model = create_model(device)
     test_dataloader = get_dataloader("test", batch_size)
@@ -337,15 +348,11 @@ def test(batch_size, device):
 
 if __name__ == '__main__':
 
-
-    
-   
     if run_mode == "test":
-          test(batch_size, device)
+        test(batch_size, device)
     else:
-          with open(output_dir_path + 'description.txt', 'w') as f:
-              f.write(description)
-              f.close()
+        with open(output_dir_path + 'description.txt', 'w') as f:
+            f.write(description)
+            f.close()
 
-          training_loop(batch_size, device)
-
+        training_loop(batch_size, device)
